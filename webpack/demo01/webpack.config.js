78c71cb0f27c1daa4891ce971ea77266c4a85ab4
XTZ-01801232210\Administrator <1009140297@qq.com>
2019-02-24 23:39:15 +0800

    添加新demo

---
+++ webpack/demo01/webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const htmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    devServer: {
+        hot: true,
+        contentBase: path.join(__dirname, 'dist'),
+        port: 3002,
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            template: "./index.html",
+            filename: "index.html",
+            inject: true,
+            hash: true,
+            chunksSortMode: 'none' //如使用webpack4将该配置项设置为'none'
+        }),
+    ],
+};
